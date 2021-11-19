47e85e8f031887e64f46eb29e18c7163963094dc
nitta13 <nittaamandah265@gmail.com>
2021-11-19 07:56:16 +0700

    versi pertama

---
+++ index.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+ <title>Nitta</title>
+  </head>
+  <body>
+      <div class="container">
+
+          <!-- ini adalah pembuka navbar -->
+          <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+              <div class="container-fluid">
+                  <a class="navbar-brand" href="#">Nitta</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#"> Halaman
+                                    
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Alamat</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Hobi</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Cita - Cita</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <!-- ini adalah penutup navbar -->
+                <!-- content1 -->
+                <div class="container">
+  <div class="row">
+    <div class="col-4">
+    <div class="card mx-auto" style="width: 18rem;">
+  <img src="assets/img/nitta.jpg" class="card-img-top" style="width: 9rem;">
+  <div class="card-body">
+    <p class="card-text">Nitta</p>
+  </div>
+</div>
+
+    </div>
+    <div class="col-8">
+      <h1> Selamat datang di website saya </h1>
+      <p> hai konsep pelangi yang 7 warna sebenarnya dinyanyian terucap ada 4 warna merah, kuning, hijau dan biru sisanya kebawa arus </p>
+    </div>
+  </div>
+</div>
+            </div>
+    
+</form>
+   <!-- Link Javascript -->
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
